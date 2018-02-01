59b2ef3216de02f1530865cd047b38d835b78f7d
Charles d_Oiron <doironcharles@gmail.com>
2018-02-01 12:53:34 +0100

    🔥

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat'); //compile js plugins into one file
 var concatCss = require('gulp-concat-css');//compile css plugins into one file
 var watch = require('gulp-watch'); //sass compile to css
 
+gulp.task('sass', function () {
     return gulp.src('html/scss/**/*.scss')
             .pipe(sass().on('error', sass.logError))
             .pipe(gulp.dest('html/css/'));
